cbbefcbf0bcdbd269169b32a41e4752ccc8879d2
Josh McGrath <joshuawmcgrath@gmail.com>
2019-02-04 11:16:08 -0600

    cleanup and color changes to viz

--- visualize.py
+++ visualize.py
@@ -1,8 +1,8 @@
 from PIL import ImageDraw, Image
 from xml_utils import parse_xml
-gt_color = "#f00"
+gt_color = "#00f"
 pred_color = "#0f0"
-corrected_color = "#00f"
+corrected_color = "#f00"
 
 def get_rectangles(xml_path):
     classes, x1, y1, x2, y2 = parse_xml(xml_path)
@@ -25,7 +25,7 @@ def draw_xml(img, xml_path, color):
 
 def draw(img_path, pred_path, gt_path, corrected_path, out_path):
     im = Image.open(img_path)
-    draw_xml(im, pred_path, pred_color)
+    #draw_xml(im, pred_path, pred_color)
     # draw_xml(im, gt_path, gt_color)
     draw_xml(im, corrected_path, corrected_color)
     im.save(out_path, "PNG")
